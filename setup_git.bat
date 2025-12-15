89b3fe6dfc32309828ec3bc51a604c14174da845
Hellk1per <ez241205@mail.ru>
2025-12-16 00:24:56 +0300

    Удалены лишние файлы

--- setup_git.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\Егор Алексеевич\MusicBingo"
-git init
-git add .
-git commit -m "Initial commit for MusicBingo game"
-git remote add origin https://github.com/hellkiper/MusicBingo.git
-git push -u origin main
